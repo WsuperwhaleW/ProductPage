1c5c8dfb5e02cb7dc33cac5d68366a09637eeaa4
whale <Whale@DESKTOP-8C9QGHK.(none)>
2024-05-14 14:35:03 +0700

    pagination

--- angular-workshop-app/src/app/product-list/product-list.component.ts
+++ angular-workshop-app/src/app/product-list/product-list.component.ts
@@ -1,19 +1,54 @@
 import { Component } from '@angular/core';
 import { ProductService } from '../product.service';
-import { NgFor } from '@angular/common';
+import { NgFor, NgIf } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css'],
-  imports:[NgFor,RouterModule],
+  imports: [NgFor, NgIf, RouterModule],
   standalone: true
 })
 export class ProductListComponent {
   products: any[];
+  paginatedProducts?: any[];
+  currentPage: number;
+  itemsPerPage: number;
 
   constructor(private productService: ProductService) {
     this.products = this.productService.getProducts();
+    this.itemsPerPage = 12;  // Set the number of items per page
+    this.currentPage = 1;
+    this.updatePaginatedProducts();
+  }
+
+  updatePaginatedProducts(): void {
+    const startIndex = (this.currentPage - 1) * this.itemsPerPage;
+    const endIndex = startIndex + this.itemsPerPage;
+    this.paginatedProducts = this.products.slice(startIndex, endIndex);
+  }
+
+  goToPage(page: number): void {
+    this.currentPage = page;
+    this.updatePaginatedProducts();
+  }
+
+  nextPage(): void {
+    if (this.currentPage < this.totalPages) {
+      this.currentPage++;
+      this.updatePaginatedProducts();
+    }
+  }
+
+  previousPage(): void {
+    if (this.currentPage > 1) {
+      this.currentPage--;
+      this.updatePaginatedProducts();
+    }
+  }
+
+  get totalPages(): number {
+    return Math.ceil(this.products.length / this.itemsPerPage);
   }
 }
